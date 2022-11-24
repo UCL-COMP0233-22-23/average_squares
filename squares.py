eaede7d725922ed3c1454fbddd0b81e0a88a6f18
Marcell Kovacs <ma7812@me.com>
2022-11-24 11:30:06 +0000

    Adding argument parsing to average_function

--- squares.py
+++ squares.py
@@ -1,5 +1,5 @@
 """Computation of weighted average of squares."""
-
+from argparse import ArgumentParser
 
 def average_of_squares(list_of_numbers, list_of_weights=None):
     """ Return the weighted average of a list of values.
@@ -53,8 +53,16 @@ def convert_numbers(list_of_strings):
 if __name__ == "__main__":
     numbers_strings = ["1","2","4"]
     weight_strings = ["1","1","1"]        
-    
-    numbers = convert_numbers(numbers_strings)
+    parser = ArgumentParser(description='Input list of numbers to average over')
+    parser.add_argument('number_list', nargs="*", type=str)
+
+    #numbers = convert_numbers(numbers_strings)
+    arguments = parser.parse_args()
+    print(arguments)
+    numbers = convert_numbers(arguments.number_list)
+    assert (len(numbers)  == len(weights))
+    #assert (type(numbers) == float).all()
+    print('terminal input: ', numbers)
     weights = convert_numbers(weight_strings)
     
     result = average_of_squares(numbers, weights)
