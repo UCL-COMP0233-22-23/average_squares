3bda9537ca973d36f050f0f7a287e6a934b266a6
Chongfeng Ling <ling.chongfeng@outlook.com>
2022-11-27 16:00:42 +0000

    ✨ feat(*): argparse input

--- squares.py
+++ squares.py
@@ -1,5 +1,5 @@
 """Computation of weighted average of squares."""
-
+from argparse import ArgumentParser
 
 def average_of_squares(list_of_numbers, list_of_weights=None):
     """ Return the weighted average of a list of values.
@@ -50,13 +50,16 @@ def convert_numbers(list_of_strings):
     return [float(number_string) for number_string in all_numbers]
 
 
-if __name__ == "__main__":
-    numbers_strings = ["1","2","4"]
-    weight_strings = ["1","1","1"]        
-    
-    numbers = convert_numbers(numbers_strings)
-    weights = convert_numbers(weight_strings)
-    
-    result = average_of_squares(numbers, weights)
-    
+def process():
+    parser = ArgumentParser(description="Computation of weighted average of squares.")
+
+    parser.add_argument('numbers_strings')
+    parser.add_argument('--weight_strings', '-w')
+
+    arguments = parser.parse_args()
+
+    result = average_of_squares(convert_numbers(arguments.numbers_strings))
     print(result)
+
+if __name__ == "__main__":
+    process()
